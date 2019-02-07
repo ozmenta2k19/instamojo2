a2e591aefb3760ab7c89ed02d8f58933c0b5312a
prasanna-pt2588 <prasanna-pt2588@prasanna-pt2588.csez.zohocorpin.com>
2019-02-07 10:31:27 +0530

    commit 12

--- index.php
+++ index.php
@@ -23,9 +23,9 @@
     <input class="w3-input w3-border" placeholder="Your Email ID" name="email" type="text" required></p>
     <p>      
     <label class="w3-text-black"><b>Amount</b></label>
-    <input class="w3-input w3-border" value="513" readonly name="amount" type="number" required></p>
+    <input class="w3-input w3-border" value="500" readonly name="amount" type="number" required></p>
     <p>
-    <input type="submit" name='submit' class="w3-btn w3-blue" value='Pay now 513 (Rs 500 + Rs13 transaction fee)'></p>
+    <input type="submit" name='submit' class="w3-btn w3-blue" value='Pay now RS 500'></p>
   </form>
 </div>
 
